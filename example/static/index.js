0ce565f03cd0defc2ed2688a6e8d11fc4a0c15b7
maoyonglong <githubMYL@163.com>
2020-02-12 21:58:39 +0800

    notification

--- example/static/index.js
+++ example/static/index.js
@@ -1,12 +1,22 @@
-const range = new uiKit.Range(document.querySelector('.range'), {
-  value: 0,
-  scope: [0, 10000],
-  step: 2000
+window.addEventListener('load', function () {
+  const range = new uiKit.Range(document.querySelector('.range'), {
+    value: 0,
+    scope: [0, 100],
+    step: 20
+  })
+  
+  const swit = new uiKit.Switch(document.querySelector('.switch'), {
+    value: false
+  })
+  
+  const progress = new uiKit.Progress(document.querySelector('.progress'), {
+    value: 10
+  })
+  
+  const notification = new uiKit.Notification(document.querySelector('.notification'), {
+    direction: 'right'
+  })
+  document.querySelector('#notification-trigger').addEventListener('click', function () {
+    notification.toggle()
+  })
 })
-
-range.init()
-
-const swit = new uiKit.Switch(document.querySelector('.switch'), {
-  value: false
-})
-swit.init()
